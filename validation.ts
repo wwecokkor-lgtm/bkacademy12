0bf48b490c73b7c3fe841658221da40db1c1b11e
wwecokkor-lgtm <wwecokkor@gmail.com>
2026-02-09 16:33:09 +0600

    Add files via upload

---
+++ validation.ts
@@ -0,0 +1,16 @@
+
+export const validateEmail = (email: string): boolean => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+export const validatePassword = (password: string): { minLength: boolean; strength: boolean } => {
+  const minLength = password.length >= 8;
+  const hasUppercase = /[A-Z]/.test(password);
+  const hasNumber = /[0-9]/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  
+  const strength = hasUppercase && hasNumber && hasSpecialChar;
+  
+  return { minLength, strength };
+};
